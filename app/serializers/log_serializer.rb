b7a2bb99a281fcef7dc19a6bdc4c6755a07e2637
V10LET <violet.wmoon@gmail.com>
2018-11-26 12:59:45 -0600

    updated log serializer

--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,7 +1,8 @@
 class LogSerializer < ActiveModel::Serializer
     belongs_to :user
+
     attributes :id, :mental_rating, :mental_note,
         :emotional_rating, :emotional_note,
         :physical_rating, :physical_note,
-        :spiritual_rating, :spiritual_note
+        :spiritual_rating, :spiritual_note, :created_at
 end
