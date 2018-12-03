4d95b9fe1f0c167ed3b13e6ba5d8d680de4ae6e5
V10LET <violet.wmoon@gmail.com>
2018-12-03 15:26:02 -0600

    potential active storage

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::UsersController < ApplicationController
     def show
         @user = User.find_by_id(params[:id])
         render json: @user
+        # {user: @user, img_url: url_for(@user.image) }
     end
 
     def login
